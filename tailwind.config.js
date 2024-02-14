ddd77c09a5e4d48d7b20b74b569b650a988650cd
Lee Connelly <lee@leeconnelly.dev>
2024-02-14 00:40:50 +0000

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ export default {
       fontFamily: {
         sans: ["Space Mono", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        blue: "#26C2AE",
+        "light-blue": "#F3F9FA",
+        green: "#00474B",
+        "light-green": "#9EBBBD",
+        teal: "#26C2AE",
+      },
     },
   },
   plugins: [],
